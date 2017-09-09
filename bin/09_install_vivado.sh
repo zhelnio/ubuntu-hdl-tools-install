7ffc88e04338f5f0401df4bcb3d7f7b78a99b5f1
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-09-09 23:13:31 +0300

    path bugfix

--- bin/09_install_vivado.sh
+++ bin/09_install_vivado.sh
@@ -14,4 +14,4 @@ echo "Please use $TARGET_FOLDER for your installation"
 chmod u+x $DIST_DIR/$DIST_NAME
 $DIST_DIR/$DIST_NAME
 
-sudo bash -c "echo 'PATH=\$PATH':$TARGET_FOLDER/Vivado/2017.2/bin/ > /etc/profile.d/mips-mti-elf.sh"
+sudo bash -c "echo 'PATH=\$PATH':$TARGET_FOLDER/Vivado/2017.2/bin/ > /etc/profile.d/vivado-2017.2.sh"
