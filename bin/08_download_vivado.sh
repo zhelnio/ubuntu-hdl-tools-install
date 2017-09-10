b964522316c8a17740caf5b824e0ecf4ff7343b7
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-09-11 00:59:02 +0300

    refactoring

--- bin/08_download_vivado.sh
+++ bin/08_download_vivado.sh
@@ -2,8 +2,10 @@
 
 # Please get your download URL from https://www.xilinx.com/support/download.html
 # Auth on Xilinx site is required
-# The simple way to get this link is to start download in browser and then copy the URL of downloading file
-DOWNLOAD_URL="<insert your link from https://www.xilinx.com/support/download.html>"
+# The simple way to get this link is to start download 'Xilinx_Vivado_SDK_2017.2_0616_1_Lin64.bin' 
+# in browser and then copy the URL of downloading file
+
+DOWNLOAD_URL="<insert your link>"
 
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 DIST_DIR=$SCRIPTPATH/../pkg
