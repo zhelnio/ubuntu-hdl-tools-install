62cf99bc4b0dbbd9df7d90f402613282233359d5
zhelnio <zhelniosl@yandex.ru>
2017-09-11 10:35:13 +0300

    mars added

---
+++ bin/10_install_mars.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TARGET_FOLDER=/opt/mars
+
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+DIST_DIR=$SCRIPTPATH/../pkg
+
+mkdir -p $DIST_DIR
+cd $DIST_DIR
+
+wget http://courses.missouristate.edu/KenVollmar/mars/MARS_4_5_Aug2014/Mars4_5.jar
+
+cd -
+
+CUR_USER=`whoami`
+sudo mkdir -p $TARGET_FOLDER
+sudo cp $DIST_DIR/Mars4_5.jar $TARGET_FOLDER
+
+sudo bash -c "echo alias mars=\'java -jar $TARGET_FOLDER/Mars4_5.jar\' > /etc/profile.d/mars-mips.sh"
+
+. /etc/profile.d/mars-mips.sh
