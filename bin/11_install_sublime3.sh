0f46d7dae788e0a10ee11b423152ad3bb7995278
Stanislav Zhelnio <zhelniosl@yandex.ru>
2018-04-13 17:54:38 +0300

    sublime install refactoring

--- bin/11_install_sublime3.sh
+++ bin/11_install_sublime3.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
-DIST_DIR=$SCRIPTPATH/../pkg
-
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
