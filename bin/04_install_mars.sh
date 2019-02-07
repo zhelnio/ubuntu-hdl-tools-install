081692bf0a62484511d41d909dbc9d8460ea66ea
Stanislav Zhelnio <zhelniosl@yandex.ru>
2019-02-07 13:06:41 +0300

    issue #1 fix

--- bin/04_install_mars.sh
+++ bin/04_install_mars.sh
@@ -16,6 +16,12 @@ fi
 # search for package or download it
 PROGRAM_JAR=`check_and_download $MARS_URL $DOWNLOAD_DIR $MARS_PKG_NAME`
 
+# install default java if it is not installed
+if ! [ -x "$(command -v java)" ]; then
+  echo 'Java not found. Installing the default one' >&2
+  sudo apt install default-jre
+fi
+
 # install 
 sudo mkdir -p $MARS_PATH
 sudo cp $PROGRAM_JAR $MARS_PATH
@@ -23,5 +29,6 @@ sudo cp $PROGRAM_JAR $MARS_PATH
 # write the PATH
 JAR=$(basename $PROGRAM_JAR)
 echo "alias mars='java -jar $MARS_PATH/$JAR'" >> ~/.bash_aliases
+source ~/.bash_aliases
 
 echo 'MARS installed successfully'
